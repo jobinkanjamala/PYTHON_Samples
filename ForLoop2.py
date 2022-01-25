087f15cae2359674f13bb84c06bd6c56ac980604
jobinkanjamala <88285170+jobinkanjamala@users.noreply.github.com>
2022-01-25 10:33:43 +0530

    Create ForLoop2.py

---
+++ ForLoop2.py
@@ -0,0 +1,12 @@
+#Python sample code 
+# two methoids for the FOR Loop in Python
+
+for number in range(1,5):
+    print("current number is  : ",number)
+print ("......................")
+for number in range(1,7,2):
+    print("current number is : ",number)
+print("--------------------")
+for number in range(5,0,-1):
+    print("current number is : ",number)
+    
