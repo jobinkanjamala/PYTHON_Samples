7ad3bb5ce9fd8e069b7ed23a6bf6b117e4cba013
jobinkanjamala <88285170+jobinkanjamala@users.noreply.github.com>
2022-01-25 17:09:21 +0530

    Update squre_by_function.py

--- squre_by_function.py
+++ squre_by_function.py
@@ -1,3 +1,4 @@
+#Write a Python function, find_square() that accepts an integer number n and returns the square of n. Invoke the function and display the square of the number. 
 #Squre of a number with a function execution
 def squre (a):
     return a*a
