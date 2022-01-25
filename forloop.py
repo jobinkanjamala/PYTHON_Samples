eb6e8c92e67b81a5f0786f83a4012c476aecc1c2
jobinkanjamala <88285170+jobinkanjamala@users.noreply.github.com>
2022-01-25 10:06:45 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,12 @@
+#Python sample code 
+# two methoids for the For Loop in Python
+
+print ("method 1 \n-------------")
+for number in 1,2,3,4,5,6,7,8,9:
+    print("number is",number)
+print("\n method 2\n------------")
+start=1
+inc=2
+end=12
+for number in range(start,end,inc):
+    print("number is : ",number)
